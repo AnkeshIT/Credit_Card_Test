b655dc54f2f0de7dea319a743be4c076a2fd3209
Ankesh <b.techitankesh@gmail.com>
2021-12-03 11:34:28 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { render } from 'react-dom'
+import Styles from './components/Styles'
+import { Form, Field } from 'react-final-form'
+import Card from './components/card'
+import {
+  formatCreditCardNumber,
+  formatCVC,
+  formatExpirationDate
+} from './components/cardFilds'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const onSubmit = async values => {
+  await sleep(300)
+  window.alert(JSON.stringify(values, 0, 2))
+}
+
+const App = () => (
+  <Styles>
+    
+    <h2>Credit Card</h2>
+    <Form
+      onSubmit={onSubmit}
+      render={({
+        handleSubmit,
+        form,
+        submitting,
+        pristine,
+        values,
+        active
+      }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <Card
+              number={values.number || ''}
+              name={values.name || ''}
+              expiry={values.expiry || ''}
+              cvc={values.cvc || ''}
+              focused={active}
+            />
+            <div>
+              <Field
+                name="number"
+                component="input"
+                type="text"
+                pattern="[\d| ]{12,16}"
+                //lengths="[16]"
+                placeholder="Card Number"
+                format={formatCreditCardNumber}
+              />
+            </div>
+            <div>
+              <Field
+                name="name"
+                component="input"
+                type="text"
+                placeholder="Name"
+              />
+            </div>
+            <div>
+              <Field
+                name="expiry"
+                component="input"
+                type="text"
+                pattern="\d\d/\d\d"
+                placeholder="Valid Thru"
+                format={formatExpirationDate}
+              />
+              <Field
+                name="cvc"
+                component="input"
+                type="text"
+                pattern="\d{3,4}"
+                placeholder="CVC"
+                format={formatCVC}
+              />
+            </div>
+            <div className="buttons">
+              <button type="submit" disabled={submitting}>
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={form.reset}
+                disabled={submitting || pristine}
+              >
+                Reset
+              </button>
+            </div>
+            <h2>Data</h2>
+            <pre>{JSON.stringify(values, 0, 2)}</pre>
+          </form>
+        )
+      }}
+    />
+  </Styles>
+)
+
+render(<App />, document.getElementById('root'))
+
+// export default App;
